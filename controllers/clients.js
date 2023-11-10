dccbcbd14f80f79e696f0e9f563551a9733cdfc8
Jeferson de A. Damasceno <jeferson.damasceno@fastsolucoes.com.br>
2023-11-10 15:43:38 -0300

    feat: ajuste no formato de retorno da lista de clientes

--- controllers/clients.js
+++ controllers/clients.js
@@ -3,13 +3,21 @@ const request = require('request');
 const router = express.Router();
 
 const clientsList = (req, res) => {
-  request.get("https://fakerapi.it/api/v1/companies?_quantity=20", (err, response, body) => {
-    //tratamento de erro quando o statusCode for diferente de 200 (sucesso)
+  request.get("https://fakerapi.it/api/v1/persons?_quantity=20", (err, response, body) => {
+    const dataBody = {
+      total: 0,
+      clients: []
+    }
+
     if(response.statusCode != 200 || err){
       res.status(response.statusCode).json(JSON.parse(body))
     }
 
-    res.json(JSON.parse(body))
+    body = JSON.parse(body);
+    dataBody.total = body.total;
+    dataBody.clients = body.data;
+
+    return res.json(dataBody)
   })
 }
 
